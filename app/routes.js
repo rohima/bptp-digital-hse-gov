238bbfaf6b87749f70e01c242baf39b8c21f4882
monsterlimo <mattnixon79@googlemail.com>
2023-12-28 15:27:09 +0000

    added crd1 label change pages and basic routing

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ router.use('/crd1-active', require('./views/crd1-active/routes'));
 // router.use('/crd1-claim-label', require('./views/crd1-claim-label/routes'));
 // router.use('/crd1-data', require('./views/crd1-data/routes'));
 // router.use('/crd1-formula', require('./views/crd1-formula/routes'));
-// router.use('/crd1-label', require('./views/crd1-label/routes'));
+router.use('/crd1-label', require('./views/crd1-label/routes'));
 // router.use('/crd1-mrl', require('./views/crd1-mrl/routes'));
 // router.use('/crd1-packaging', require('./views/crd1-packaging/routes'));
 // router.use('/crd1-use', require('./views/crd1-use/routes'));
