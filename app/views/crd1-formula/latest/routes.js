2d53bf573d5ebac59bd7862c7eb36f05189dbb5d
monsterlimo <mattnixon79@googlemail.com>
2023-12-29 10:09:44 +0000

    added crd1 formula

--- app/views/crd1-formula/latest/routes.js
+++ app/views/crd1-formula/latest/routes.js
@@ -9,7 +9,7 @@ const router = express.Router()
 router.post('/existing-job-number', function (req, res) {
   
   let existingJobNumber = req.session.data.existingJobNumber;
-  res.redirect('send-verify-code');
+  res.redirect('application-name-contact');
 
 })
 
@@ -146,17 +146,6 @@ router.post('/task-list', function (req, res) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 // ----------------- FIND PRODUCT ----------------- //
 
 // find-search
@@ -177,11 +166,189 @@ router.post('/find-results', function (req, res) {
 
 // find-confirm
 
-router.post('/find-results', function (req, res) {  
+router.post('/find-confirm', function (req, res) {  
   let findConfirm = req.session.data.findConfirm;
-  res.redirect('find-confirm');
+  res.redirect('formula-is-csv-upload');
 
 })
+
+
+
+// ----------------- FORMULATION COMPOSITION ----------------- //
+
+// formula-is-csv-upload
+
+router.post('/formula-is-csv-upload', function (req, res) {
+  let isCsvUpload = req.session.data.isCsvUpload;
+
+  if (isCsvUpload == "Yes") {
+    res.redirect('formula-csv-1');
+  } else {
+    res.redirect('formula-type-add');
+  }
+})
+
+// formula-csv-1
+
+router.post('/formula-csv-1', function (req, res) {
+  res.redirect('formula-csv-3');
+})
+
+// formula-csv-2 - NOT USED
+
+router.post('/formula-csv-2', function (req, res) {
+  res.redirect('formula-csv-3');
+})
+
+// formula-csv-3
+
+router.post('/formula-csv-3', function (req, res) {
+  res.redirect('documents-reference-product-risk-areas');
+})
+
+// ----------------- FORMULATION MANUAL ----------------- //
+
+// formula-type-add
+router.post('/formula-type-add', function (req, res) {
+  let formulaTypeAdd = req.session.data.formulaTypeAdd;
+  if (formulaTypeAdd == "Co Formulant") {
+    res.redirect('formula-type-add-co-formulant-details');
+  } else {
+    res.redirect('formula-type-add-active-details');
+  }
+})
+
+// formula-type-add-active-details
+router.post('/formula-type-add-active-details', function (req, res) {
+  res.redirect('formula-composition-add-another-1');
+})
+
+// formula-composition-add-another-1
+router.post('/formula-composition-add-another-1', function (req, res) {
+  let addAnotherFormulaComposition = req.session.data.addAnotherFormulaComposition
+
+  if (addAnotherFormulaComposition == "Yes") {
+    res.redirect('formula-type-add');
+  } else {
+    res.redirect('formula-risk-product');
+  }
+})
+
+// formula-type-add-co-formulant-details
+router.post('/formula-type-add-co-formulant-details', function (req, res) {
+  res.redirect('formula-substance-components');
+})
+
+// formula-substance-trade-name - NOT USED
+router.post('/formula-substance-trade-name', function (req, res) {
+  res.redirect('formula-substance-components');
+})
+
+// formula-substance-components
+router.post('/formula-substance-components', function (req, res) {
+  let formulaSubstanceNumberComponents = req.session.data.formulaSubstanceNumberComponents;
+  let formulaSubstanceMixture = req.session.data.formulaSubstanceMixture;
+  if (formulaSubstanceMixture == "Yes") {
+    res.redirect('formula-type-add-component');
+  } else {
+    res.redirect('formula-composition-add-another');
+  }
+  
+})
+
+// formula-type-add-component
+router.post('/formula-type-add-component', function (req, res) {
+  res.redirect('formula-substance-component-add-another');
+})
+
+
+// formula-substance-component-add-another
+
+router.post('/formula-substance-component-add-another', function (req, res) {
+  let addAnotherSubstanceComponent = req.session.data.addAnotherSubstanceComponent;
+  
+  if (addAnotherSubstanceComponent == "Yes") {
+    res.redirect('formula-type-add-component');
+  } else {
+    res.redirect('formula-substance-add-another');
+  }
+})
+
+// formula-substance-component-remove
+router.post('/formula-substance-component-remove', function (req, res) {
+  res.redirect('formula-substance-component-add-another');
+})
+
+// formula-substance-add-another
+router.post('/formula-substance-add-another', function (req, res) {
+  res.redirect('formula-risk-product');
+})
+
+// formula-substance-remove
+router.post('/formula-substance-remove', function (req, res) {
+  res.redirect('formula-substance-add-another');
+})
+
+// formula-composition-add-another
+router.post('/formula-composition-add-another', function (req, res) {
+  res.redirect('formula-risk-product');
+})
+
+
+
+
+// formula-risk-product
+
+router.post('/formula-risk-product', function (req, res) {
+  let productRisk = req.session.data.productRisk;
+  if (productRisk == "No") {
+    res.redirect('documents-reference-product-risk-areas');
+  } else {
+    res.redirect('formula-risk-upload');
+  }
+
+})
+
+// formula-risk-upload
+
+router.post('/formula-risk-upload', function (req, res) {
+  
+  let productRiskUpload = req.session.data.productRiskUpload;
+  res.redirect('documents-reference-product-risk-areas');
+
+})
+
+
+
+// ----------------- SPECIAL ASSESSMENT AREA ----------------- //
+
+// documents-reference-product-risk-areas
+
+router.post('/documents-reference-product-risk-areas', function (req, res) {  
+  let documentsReferenceProductRiskAreas = req.session.data.documentsReferenceProductRiskAreas;
+  res.redirect('placeholder-no-change');
+
+})
+
+
+// ----------------- PLACEHOLDER PAGE ----------------- //
+
+// Affirm Nothing else has changed / not required
+
+router.post('/placeholder-no-change', function (req, res) {  
+  let placeholderPage = req.session.data.placeholderPage;
+  res.redirect('placeholder-eamus-affected');
+})
+
+// confirm whether EAMUs are affected
+
+router.post('/placeholder-eamus-affected', function (req, res) {  
+  let placeholderPage = req.session.data.placeholderPage;
+  res.redirect('check-answers');
+})
+
+
+
 
 // ----------------- PLACEHOLDER PAGE ----------------- //
 
